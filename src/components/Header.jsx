6bb4c986717fdc2a49d980e18cc95495f4592f4e
EthanChenYen-Peng <ypcethan@gmail.com>
2022-05-05 17:34:55 +1000

    setup eslint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import logo from '../../images/logo.svg'
 function Header() {
   return (
-    <div className='flex px-2'>
+    <div className="flex py-3 px-2">
       <div>Logo</div>
       <img src={logo} />
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
