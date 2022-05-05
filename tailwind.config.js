5572d3266fa34dc5a57fce85864cd16c0b51e5a6
EthanChenYen-Peng <ypcethan@gmail.com>
2022-05-05 17:27:01 +1000

    setup custom tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    screen: {
+      mobile: "375px",
+      desktop: "1440px",
+    },
+    colors: {
+      "almost-white": "hsl(0, 0%, 98%)",
+      "medium-gray": "hsl(0, 0%, 41%)",
+      "almost-black": "hsl(0, 0%, 8%)",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
