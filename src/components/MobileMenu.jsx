6c6fd0c2521f71f21fc26dd9091a66a20860134e
EthanChenYen-Peng <ypcethan@gmail.com>
2022-05-06 13:49:58 +1000

    refactor MobileMenu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,26 +1,21 @@
-function MobileOverlay({ menuOpen }) {
+function MobileOverlay({ menuOpen, children }) {
   return (
     <div
       className={`${
         menuOpen ? 'fixed' : 'hidden'
       } top-0 bottom-0 right-0 left-0 bg-medium-gray`}
     >
-      <p>Mobile</p>
+      {children}
     </div>
   )
 }
 function MobileMenu({ menuOpen }) {
   return (
-    <>
-      <MobileOverlay menuOpen={menuOpen} />
-      <div
-        className={`${
-          menuOpen ? 'fixed' : 'hidden'
-        } top-0 bottom-0 right-0 w-1/2 bg-almost-white`}
-      >
+    <MobileOverlay menuOpen={menuOpen}>
+      <div className={'absolute top-0 bottom-0 right-0 w-1/2 bg-almost-white'}>
         <p>Mobile</p>
       </div>
-    </>
+    </MobileOverlay>
   )
 }
 
