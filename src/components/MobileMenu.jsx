ea4ef8622b2e087e8c56d88b72718314518c154a
EthanChenYen-Peng <ypcethan@gmail.com>
2022-05-06 16:51:32 +1000

    refactor mobile nav overlay

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,24 +1,23 @@
 import MobileDropdownLink from './MobileDropdownLink'
 
-function MobileOverlay({ menuOpen, children }) {
+function MobileOverlay({ menuOpen }) {
   return (
     <div
       className={`${
         menuOpen ? 'translate-x-0' : 'translate-x-full'
       } fixed top-0 bottom-0 right-0 left-0 bg-almost-black opacity-80 transition-transform duration-500 ease-in-out desktop:hidden`}
-    >
-      {children}
-    </div>
+    ></div>
   )
 }
 
 function MobileMenu({ menuOpen }) {
   return (
-    <MobileOverlay menuOpen={menuOpen}>
+    <>
+      <MobileOverlay menuOpen={menuOpen} />
       <div
-        className={
-          'absolute top-0 bottom-0 right-0 w-8/12 bg-almost-white px-6'
-        }
+        className={`${
+          menuOpen ? 'translate-x-0' : 'translate-x-full'
+        } fixed top-0 bottom-0 right-0 w-8/12 bg-almost-white px-6 transition-transform duration-500 desktop:hidden`}
       >
         <ul className="mt-32 flex flex-col gap-5 text-xl text-almost-black">
           <MobileDropdownLink title="Features" />
@@ -26,7 +25,7 @@ function MobileMenu({ menuOpen }) {
           <li>Features</li>
         </ul>
       </div>
-    </MobileOverlay>
+    </>
   )
 }
 
