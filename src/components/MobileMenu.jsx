807a7d4f4416c82aa57f1d031e8ba8c6626a369b
EthanChenYen-Peng <ypcethan@gmail.com>
2022-05-06 13:53:59 +1000

    add mobile menu animation

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -2,8 +2,8 @@ function MobileOverlay({ menuOpen, children }) {
   return (
     <div
       className={`${
-        menuOpen ? 'fixed' : 'hidden'
-      } top-0 bottom-0 right-0 left-0 bg-medium-gray`}
+        menuOpen ? 'translate-x-0' : 'translate-x-full'
+      } fixed top-0 bottom-0 right-0 left-0 bg-almost-black opacity-80 transition-transform duration-500 ease-in-out desktop:hidden`}
     >
       {children}
     </div>
@@ -12,7 +12,7 @@ function MobileOverlay({ menuOpen, children }) {
 function MobileMenu({ menuOpen }) {
   return (
     <MobileOverlay menuOpen={menuOpen}>
-      <div className={'absolute top-0 bottom-0 right-0 w-1/2 bg-almost-white'}>
+      <div className={'absolute top-0 bottom-0 right-0 w-8/12 bg-almost-white'}>
         <p>Mobile</p>
       </div>
     </MobileOverlay>
